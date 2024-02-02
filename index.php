d63c04eb7dd593eb693b2e21b72668d6c7ccca98
FlyBayTv <kevin.heinrich.b@gmail.com>
2024-02-02 20:26:21 +0100

    fixed bugs

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
         <div class="container-fluid bg-primary">
             <div class="container">
                 <nav class="navbar navbar-dark navbar-expand-lg py-0">
-                    <a href="index.php" class="navbar-brand">
+                    <a href="index.html" class="navbar-brand">
                         <h1 class="text-white fw-bold d-block">High<span class="text-secondary">Tech</span> </h1>
                     </a>
                     <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -67,20 +67,20 @@
                     </button>
                     <div class="collapse navbar-collapse bg-transparent" id="navbarCollapse">
                         <div class="navbar-nav ms-auto mx-xl-auto p-0">
-                            <a href="index.php" class="nav-item nav-link active text-secondary">Home</a>
-                            <a href="about.php" class="nav-item nav-link">About</a>
+                            <a href="index.html" class="nav-item nav-link active text-secondary">Home</a>
+                            <a href="about.html" class="nav-item nav-link">About</a>
                             <a href="service.php" class="nav-item nav-link">Services</a>
-                            <a href="project.php" class="nav-item nav-link">Projects</a>
+                            <a href="project.html" class="nav-item nav-link">Projects</a>
                             <div class="nav-item dropdown">
                                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                                 <div class="dropdown-menu rounded">
-                                    <a href="blog.php" class="dropdown-item">Our Blog</a>
-                                    <a href="team.php" class="dropdown-item">Our Team</a>
-                                    <a href="testimonial.php" class="dropdown-item">Testimonial</a>
-                                    <a href="404.php" class="dropdown-item">404 Page</a>
+                                    <a href="blog.html" class="dropdown-item">Our Blog</a>
+                                    <a href="team.html" class="dropdown-item">Our Team</a>
+                                    <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                                    <a href="404.html" class="dropdown-item">404 Page</a>
                                 </div>
                             </div>
-                            <a href="contact.php" class="nav-item nav-link">Contact</a>
+                            <a href="contact.html" class="nav-item nav-link">Contact</a>
                         </div>
                     </div>
                     <div class="d-none d-xl-flex flex-shirink-0">
@@ -759,6 +759,7 @@
         </div>
         <!-- Contact End -->
 
+
         <?php include'src/footer.php'  ?>
 
 
@@ -776,7 +777,7 @@
         <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 
         <!-- Template Javascript -->
-        <script src="js/main.js"></script>
+        <script src="src/js/main.js"></script>
     </body>
 
 </html>
