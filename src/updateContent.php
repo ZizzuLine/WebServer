3509495f188a97d091b13e3e36803d58a19e69d8
God23 <achraferriyahi23@gmail.com>
2024-03-20 20:45:30 +0100

    added "{" to resolve the error

--- src/updateContent.php
+++ src/updateContent.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
+if ($_SERVER["REQUEST_METHOD"] == "GET") { 
     if (isset($_GET["var1"]) && isset($_GET["var2"])) {
         $var1 = $_GET["var1"];
         $var2 = $_GET["var2"];
