268c0dc6cfdcce9fdf2a081c2d25221c3eb48c94
God23 <achraferriyahi23@gmail.com>
2024-03-20 20:38:44 +0100

    risoluzione over del mouse sulla nav

--- src/navbar.php
+++ src/navbar.php
@@ -1,4 +1,3 @@
-
 <div class="container-fluid bg-primary">
             <div class="container">
                 <nav class="navbar navbar-dark navbar-expand-lg py-0">
@@ -10,11 +9,35 @@
                     </button>
                     <div class="collapse navbar-collapse bg-transparent" id="navbarCollapse">
                         <div class="navbar-nav ms-auto mx-xl-auto p-0">
-                            <a href="../index.php" class="nav-item nav-link active text-secondary">Home</a>
-                            <a href="about.php" class="nav-item nav-link">About</a>
-                            <a href="service.php" class="nav-item nav-link">Services</a>
-                            <a href="table.php" class="nav-item nav-link">Tabel</a>
-                            <a href="project.php" class="nav-item nav-link">Projects</a>
+
+                            <a href="../index.php" class="nav-item nav-link">Home</a>
+                            <?php
+                                $current_page = $_SERVER['REQUEST_URI'];
+                                // About
+                                if ($current_page == "/zizzuline/Webserver/src/about.php") {
+                                    echo '<a href="about.php" class="nav-item nav-link active text-secondary">About</a>';
+                                } else {
+                                    echo '<a href="about.php" class="nav-item nav-link">About</a>';
+                                }
+                                // Services
+                                if ($current_page == "/zizzuline/Webserver/src/service.php") {
+                                    echo '<a href="service.php" class="nav-item nav-link active text-secondary">Services</a>';
+                                } else {
+                                    echo '<a href="service.php" class="nav-item nav-link">Services</a>';
+                                }
+                                // Table
+                                if ($current_page == "/zizzuline/Webserver/src/table.php") {
+                                    echo '<a href="table.php" class="nav-item nav-link active text-secondary">Table</a>';
+                                } else {
+                                    echo '<a href="table.php" class="nav-item nav-link">Table</a>';
+                                }
+                                // Projects
+                                if ($current_page == "/zizzuline/Webserver/src/project.php") {
+                                    echo '<a href="project.php" class="nav-item nav-link active text-secondary">Projects</a>';
+                                } else {
+                                    echo '<a href="project.php" class="nav-item nav-link">Projects</a>';
+                                }
+                            ?>
                             <div class="nav-item dropdown">
                                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                                 <div class="dropdown-menu rounded">
@@ -24,7 +47,13 @@
                                     <a href="404.php" class="dropdown-item">404 Page</a>
                                 </div>
                             </div>
-                            <a href="contact.php" class="nav-item nav-link">Contact</a>
+                            <?php
+                                if ($current_page == "/zizzuline/Webserver/src/contact.php") {
+                                    echo '<a href="contact.php" class="nav-item nav-link active text-secondary">Contact</a>';
+                                } else {
+                                    echo '<a href="contact.php" class="nav-item nav-link">Contact</a>';
+                                }
+                            ?>
                         </div>
                     </div>
                     <div class="d-none d-xl-flex flex-shirink-0">
