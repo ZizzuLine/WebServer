648672778e620016affc72e37fe0d51c8cf7394d
God23 <achraferriyahi23@gmail.com>
2024-04-10 20:31:41 +0200

    prova

---
+++ src/1prova.php
@@ -0,0 +1,14 @@
+<div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay=".5s">
+    <div class="project-item">
+        <!-- Aggiungi l'elemento a intorno all'immagine e al suo contenuto -->
+        <a href="src/bus.php" class="text-center">
+            <div class="project-img">
+                <img src="img/project-2.jpg" class="img-fluid w-100 rounded" alt="">
+                <div class="project-content">
+                    <h4 class="text-secondary">Bus</h4>
+                    <p class="m-0 text-white"></p>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
