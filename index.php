dd752a518b7455267a0b647279b26a167ed75d5e
God23 <achraferriyahi23@gmail.com>
2024-04-10 20:31:27 +0200

    add switch and cursor:pointer

--- index.php
+++ index.php
@@ -34,6 +34,7 @@
         <div class="spinner-grow text-primary" role="status"></div>
     </div>
     <!-- Spinner End -->
+
     <!-- navbar star -->
     <div class="container-fluid bg-primary">
         <div class="container">
@@ -48,9 +49,6 @@
                     <div class="navbar-nav ms-auto mx-xl-auto p-0">
                         <a href="index.php" class="nav-item nav-link active text-secondary">Home</a>
                         <a href="src/team.php" class="nav-item nav-link">Our Team</a>
-                        <!-- <a href="src/service.php" class="nav-item nav-link">Services</a> -->
-                        <!-- <a href="src/table.php" class="nav-item nav-link">Tabel</a> -->
-                        <!-- <a href="src/project.php" class="nav-item nav-link">Projects</a> -->
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Services</a>
                             <div class="dropdown-menu rounded">
@@ -94,10 +92,6 @@
     </div>
     <!-- Carousel End -->
 
-
-
-
-
     <!-- About Start -->
     <div id="introduzione" class="container-fluid py-5 my-5">
         <div class="container pt-5">
@@ -123,21 +117,39 @@
     </div>
     <!-- About End -->
 
-
     <!-- Project Start -->
     <div class="container-fluid project py-5 mb-5">
         <div class="container">
-            <div class="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style="max-width: 600px;">
-                <h5 class="text-primary">Technologie</h5>
-                <h1>Utilizzate nel progetto</h1>
+            <div class="navbar navbar-expand-lg py-0">
+                <div class="nav-item dropdown">
+                    <a style="color:black" href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Servizi</a>
+                    <div class="dropdown-menu rounded">
+                        <a href="index.php" class="dropdown-item">Pubblico
+                            <?php
+                            echo '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+                        <path d="M 22.78125 0 C 21.605469 -0.00390625 20.40625 0.164063 19.21875 0.53125 C 12.902344 2.492188 9.289063 9.269531 11.25 15.59375 L 11.25 15.65625 C 11.507813 16.367188 12.199219 18.617188 12.625 20 L 9 20 C 7.355469 20 6 21.355469 6 23 L 6 47 C 6 48.644531 7.355469 50 9 50 L 41 50 C 42.644531 50 44 48.644531 44 47 L 44 23 C 44 21.355469 42.644531 20 41 20 L 14.75 20 C 14.441406 19.007813 13.511719 16.074219 13.125 15 L 13.15625 15 C 11.519531 9.722656 14.5 4.109375 19.78125 2.46875 C 25.050781 0.832031 30.695313 3.796875 32.34375 9.0625 C 32.34375 9.066406 32.34375 9.089844 32.34375 9.09375 C 32.570313 9.886719 33.65625 13.40625 33.65625 13.40625 C 33.746094 13.765625 34.027344 14.050781 34.386719 14.136719 C 34.75 14.226563 35.128906 14.109375 35.375 13.832031 C 35.621094 13.550781 35.695313 13.160156 35.5625 12.8125 C 35.5625 12.8125 34.433594 9.171875 34.25 8.53125 L 34.25 8.5 C 32.78125 3.761719 28.601563 0.542969 23.9375 0.0625 C 23.550781 0.0234375 23.171875 0 22.78125 0 Z M 9 22 L 41 22 C 41.554688 22 42 22.445313 42 23 L 42 47 C 42 47.554688 41.554688 48 41 48 L 9 48 C 8.445313 48 8 47.554688 8 47 L 8 23 C 8 22.445313 8.445313 22 9 22 Z M 25 30 C 23.300781 30 22 31.300781 22 33 C 22 33.898438 22.398438 34.6875 23 35.1875 L 23 38 C 23 39.101563 23.898438 40 25 40 C 26.101563 40 27 39.101563 27 38 L 27 35.1875 C 27.601563 34.6875 28 33.898438 28 33 C 28 31.300781 26.699219 30 25 30 Z"></path></svg>';
+                            ?>
+                        </a>
+                        <a href="index.php" class="dropdown-item">Privato
+                            <?php
+                            echo '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+                        <path d="M 25 3 C 18.363281 3 13 8.363281 13 15 L 13 20 L 9 20 C 7.355469 20 6 21.355469 6 23 L 6 47 C 6 48.644531 7.355469 50 9 50 L 41 50 C 42.644531 50 44 48.644531 44 47 L 44 23 C 44 21.355469 42.644531 20 41 20 L 37 20 L 37 15 C 37 8.363281 31.636719 3 25 3 Z M 25 5 C 30.566406 5 35 9.433594 35 15 L 35 20 L 15 20 L 15 15 C 15 9.433594 19.433594 5 25 5 Z M 9 22 L 41 22 C 41.554688 22 42 22.445313 42 23 L 42 47 C 42 47.554688 41.554688 48 41 48 L 9 48 C 8.445313 48 8 47.554688 8 47 L 8 23 C 8 22.445313 8.445313 22 9 22 Z M 25 30 C 23.300781 30 22 31.300781 22 33 C 22 33.898438 22.398438 34.6875 23 35.1875 L 23 38 C 23 39.101563 23.898438 40 25 40 C 26.101563 40 27 39.101563 27 38 L 27 35.1875 C 27.601563 34.6875 28 33.898438 28 33 C 28 31.300781 26.699219 30 25 30 Z"></path></svg>';
+                            ?>
+                        </a>
+                    </div>
+                </div>
+                <div class="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style="max-width: 600px;">
+                    <h5 class="text-primary">Technologie</h5>
+                    <h1>Utilizzate nel progetto</h1>
+                </div>
             </div>
             <div class="row g-5">
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay=".3s">
-                    <div class="project-item">
+                    <div class="project-item" onclick="window.location.href='src/bus.php';" style="cursor:pointer;">
                         <div class="project-img">
                             <img src="img/rfid.jpg" class="img-fluid w-100 rounded" alt="">
                             <div class="project-content">
-                                <a href="#" class="text-center">
+                                <a class="text-center">
                                     <h4 class="text-secondary">Rfid</h4>
                                     <p class="m-0 text-white"></p>
                                 </a>
@@ -146,11 +158,11 @@
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay=".5s">
-                    <div class="project-item">
+                    <div class="project-item" onclick="window.location.href='src/bus.php';" style="cursor:pointer;">
                         <div class="project-img">
                             <img src="img/project-2.jpg" class="img-fluid w-100 rounded" alt="">
                             <div class="project-content">
-                                <a href="src/bus.php" class="text-center">
+                                <a class="text-center">
                                     <h4 class="text-secondary">Bus</h4>
                                     <p class="m-0 text-white"></p>
                                 </a>
@@ -159,11 +171,11 @@
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay=".7s">
-                    <div class="project-item">
+                    <div class="project-item" onclick="window.location.href='src/bus.php';" style="cursor:pointer;">
                         <div class="project-img">
                             <img src="img/project-3.jpg" class="img-fluid w-100 rounded" alt="">
                             <div class="project-content">
-                                <a href="#" class="text-center">
+                                <a class="text-center">
                                     <h4 class="text-secondary">Mobile Info</h4>
                                     <p class="m-0 text-white">Upcomming Phone</p>
                                 </a>
@@ -172,11 +184,11 @@
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay=".3s">
-                    <div class="project-item">
+                    <div class="project-item" onclick="window.location.href='src/bus.php';" style="cursor:pointer;">
                         <div class="project-img">
                             <img src="img/project-4.jpg" class="img-fluid w-100 rounded" alt="">
                             <div class="project-content">
-                                <a href="#" class="text-center">
+                                <a class="text-center">
                                     <h4 class="text-secondary">Web Development</h4>
                                     <p class="m-0 text-white">Web Analysis</p>
                                 </a>
@@ -185,11 +197,11 @@
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay=".5s">
-                    <div class="project-item">
+                    <div class="project-item" onclick="window.location.href='src/bus.php';" style="cursor:pointer;">
                         <div class="project-img">
                             <img src="img/project-5.jpg" class="img-fluid w-100 rounded" alt="">
                             <div class="project-content">
-                                <a href="#" class="text-center">
+                                <a class="text-center">
                                     <h4 class="text-secondary">Digital Marketing</h4>
                                     <p class="m-0 text-white">Marketing Analysis</p>
                                 </a>
@@ -198,11 +210,11 @@
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeIn d-flex justify-content-center align-items-center" data-wow-delay=".7s">
-                    <div class="project-item">
+                    <div class="project-item" onclick="window.location.href='src/bus.php';" style="cursor:pointer;">
                         <div class="project-img">
                             <img src="img/crittografia.jpg" class="img-fluid w-100 rounded" alt="">
                             <div class="project-content">
-                                <a href="#" class="text-center">
+                                <a class="text-center">
                                     <h4 class="text-secondary">Sicurezza</h4>
                                     <p class="m-0 text-white">di comunicazione</p>
                                 </a>
@@ -215,20 +227,15 @@
     </div>
     <!-- Project End -->
 
-
-
     <?php include 'src/footer.php'  ?>
-
-
 
     <!-- Back to Top -->
     <a href="#title" class="btn btn-secondary btn-square rounded-circle back-to-top"><i class="fa fa-arrow-up text-white"></i></a>
 
-
     <!-- JavaScript Libraries -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script> aggiunge le animazioni ì, ma allo stesso tempo rende difficile modificare il codice
+    <script src="lib/wow/wow.min.js"></script>
     <script src="lib/easing/easing.min.js"></script>
     <script src="lib/waypoints/waypoints.min.js"></script>
     <script src="lib/owlcarousel/owl.carousel.min.js"></script>
