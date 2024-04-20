0f9c71e84e97bedadf8bfc818574b0ab9cb23a41
FlyBayTv <kevin.heinrich.b@gmail.com>
2024-04-20 10:18:04 +0200

    update structur

---
+++ src/db/db_conn.php
@@ -0,0 +1,14 @@
+<?php
+
+    $servername= "localhost";
+    $username= "root";
+    $password = "";
+    $db_name = "zizzuline";
+
+    $conn = mysqli_connect($servername, $username, $password, $db_name);
+
+    if (!$conn) {
+        echo "Connection failed!";
+    }
+
+?>
