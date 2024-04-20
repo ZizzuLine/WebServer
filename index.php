8b4abcea7328293bf5417d81de0ec8f9879c08d2
FlyBayTv <kevin.heinrich.b@gmail.com>
2024-04-20 11:04:24 +0200

    fixed bug di ach

--- index.php
+++ index.php
@@ -180,19 +180,8 @@
                         <div class="project-img">
                             <img src="img/temperatura.jpg" class="img-fluid w-100 rounded" alt="">
                             <div class="project-content">
-<<<<<<< HEAD
                                 <a class="text-center">
                                     <h4 class="text-secondary">Temperatura</h4>
-=======
-
-                                <a href="src/bus.php" class="text-center">
-
-                                <a class="text-center">
-                                    <h4 class="text-secondary">Temperatura</h4>
-                                <a href="graphic.php?id_argument=id1" class="text-center" onclick="event.stopPropagation();">
-                                    <h4 class="text-secondary">Rfid</h4>
-                                    <p class="m-0 text-white"></p>
->>>>>>> 234adaeabb944bc9414f246035b78587e82131b0
                                 </a>
                             </div>
                         </div>
@@ -223,10 +212,6 @@
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> 234adaeabb944bc9414f246035b78587e82131b0
                 <div class="row justify-content-center g-5">
                     <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay=".3s">
                         <div class="project-item" onclick="window.location.href='src/bus.php';" style="cursor:pointer;">
