9b4dc20e7113e826aaee5f694c16c80111e1b5d4
FlyBayTv <kevin.heinrich.b@gmail.com>
2024-05-15 15:54:10 +0200

    modified title

--- index.php
+++ index.php
@@ -220,7 +220,7 @@
     <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container">
             <div class="text-center">
-                <h6 class="section-title bg-white text-center text-primary px-3">Testimonial</h6>
+                <h6 class="section-title bg-white text-center text-primary px-3">Details</h6>
                 <h1 class="mb-5">Punti di forza</h1>
             </div>
             <div class="owl-carousel testimonial-carousel position-relative">
